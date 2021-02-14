a2988412fb6af509b6379ef7fa3f8b3a0d56a2a5
Vidhu Kant Sharma <bokuwakanojogahoshii@yahoo.com>
2021-02-14 20:06:53 +0530

    patched with cyclelayouts (patch #7)

--- config.def.h
+++ config.def.h
@@ -50,6 +50,7 @@ static const Layout layouts[] = {
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
 	{ "|||",      col },
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -89,6 +90,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[5]} },
+	{ MODKEY,			XK_space,  cyclelayout,    {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_space,  cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} }, /**/
